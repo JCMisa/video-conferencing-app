3fac2261264748e0e30655cdeac19d7d7e95d950
JC MISA <johncarlomisa399@gmail.com>
2024-05-17 16:57:54 +0800

    Use shadcdn for pre built components, and Stream for accessing device audio and camera

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,6 +1,15 @@
 import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "LOOM",
+  description: "Video calling app",
+  icons: {
+    icon: '/icons/logo.png'
+  }
+};
 
 const HomeLayout = ({children}: {children: ReactNode}) => {
   return (
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,21 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "LOOM",
+  description: "Video calling app",
+  icons: {
+    icon: '/icons/logo.png'
+  }
+};
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
     <main>
+      <StreamVideoProvider>
         {children}
+      </StreamVideoProvider>
     </main>
   )
 }
